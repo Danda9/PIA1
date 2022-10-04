df123fe16ea7b6d4888a9f7c7910ad6060baf3a7
Danda9 <113998003+Danda9@users.noreply.github.com>
2022-10-04 14:52:21 +0200

    Verze2

--- v1.cpp
+++ v1.cpp
@@ -21,48 +21,74 @@ int main(){
         }
     } while(kontrola == true);
 
-
     std::cout << "Zadej prijmeni: ";
     std::string prijmeni;
     std::cin >> prijmeni;
 
-    std::cout<<"Zadej vysku: ";
     float vyska;
-    std::cin >> vyska;
+    kontrola = true;
+    do{
+        std::cout<<"Zadej vysku: ";
+        std::cin >> vyska;
+        if (100 < vyska && vyska < 230){
+            kontrola = false;
+        }
+        else{
+            std::cout << "Zadal jsi nespravne hodnoty!\n ";
+        }
+    } while (kontrola == true);
 
-
-    std::cout<<"Zadej vek: ";
     int vek;
-    std::cin >> vek;
-    std::cout<<"cislo: " << vek << "\n";
+    kontrola = true;
+    do{
+        std::cout<<"Zadej vek: ";
+        std::cin >> vek;
+        if (5 < vek && vek < 100){
+        kontrola = false;
+        }
+        else{
+            std::cout << "Zadal jsi nespravne hodnoty!\n ";
+        }
+    } while (kontrola == true);
+
 
 
 
     enum vzdelani {zs,ss,vs};
     std::cout << "Zadej dosazene vzdelani: 1-Zakladni 2-Stredni 3-Vysokoskolske \n";
     int skola1;
-    std::cin >> skola1;
+
     std::string skola;
 
-
-    if (skola1 == 1) {
-        skola = "zakladni";
+    do{
+        std::cin >> skola1;
+        if (skola1 == 1) {
+            skola = "zakladni";
+            kontrola = false;
 
         }
         else if (skola1 == 2) {
             skola = "stredni";
+            kontrola = false;
         }
-        else {
+        else if (skola1 == 3){
             skola = "vysokoskolske";
+            kontrola = false;
         }
+        else{
+            std::cout << "Zadal jsi nespravne hodnoty!\n ";
+            kontrola = true;
+        }
+    }while (kontrola == true);
 
-    std::cout << "vzdelani je " << skola << "\n";
+
 
     std::cout << "Jmeno: " << jmeno << "\nPrijmeni: " << prijmeni;
     std::cout << "\nVyska: " << vyska << "\nVek: " << vek << "\n";
+    std::cout << "Vzdelani je " << skola << "\n";
 
     std::ofstream zapisDoSouboru("udaje.txt");
-    zapisDoSouboru << jmeno << "\n" << prijmeni << vyska << "\n" << vek << "\n" << skola << std::endl;
+    zapisDoSouboru << jmeno << "\n" << prijmeni << "\n" << vyska << "\n" << vek << "\n" << skola << std::endl;
 
     return 0;
 
