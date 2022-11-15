03921caa8105c70ae004a6ec04063d487b3f869c
Danda9 <113998003+Danda9@users.noreply.github.com>
2022-11-15 16:26:57 +0100

    Ukol ze cviceni 8 (udaje, jejich serazeni, ulozeni)

---
+++ Udaje.h
@@ -0,0 +1,33 @@
+#ifndef UDAJE_H
+#define UDAJE_H
+#include <iostream>
+#include <vector>
+
+
+class Udaje
+{
+    public:
+        Udaje();
+        //void nacti_udaje(bool& k);
+        virtual ~Udaje();
+        std::string jmeno_out, prijmeni_out;
+        int vek_out;
+        float vyska_out;
+        bool kk();
+        bool k;
+//        std::vector info()=(4);
+
+    protected:
+
+
+    private:
+        std::string pomoc;
+        std::string jmeno(), prijmeni();
+        float pomoc1;
+        int pomoc2;
+        bool kontrola;
+        int vek();
+        float vyska();
+};
+
+#endif // UDAJE_H
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include "Udaje.h"
+
+void neco(std::vector <Udaje>& sz){
+    bool kontr = true;
+    do{
+        sz.push_back(Udaje());
+        kontr=sz[i].kk();
+        i++;
+    } while (kontr==true);
+}
+
+void serad_pr(std::vector <Udaje>& sz){
+    std::sort(sz.begin(),sz.end(),[](const Udaje& p1, const Udaje& p2) {
+        return p1.prijmeni_out < p2.prijmeni_out;
+    });
+}
+
+void uloz(std::vector <Udaje>& sz){
+    std::ofstream zapis("data.txt");
+    for(int i = 0; i<sz.size();i++){
+        zapis << sz[i].jmeno_out << " "<< sz[i].prijmeni_out << " "<< sz[i].vyska_out << " "<< sz[i].vek_out << "\n";
+    }
+}
+
+int main(){
+    int velikost = 1;
+    std::vector <Udaje> seznam;
+
+    neco(seznam, velikost);
+    serad_pr(seznam);
+    uloz(seznam);
+
+    //for(int i=0; i<seznam.size(); i++){
+      //  std::cout << seznam[i].jmeno_out;
+    //}
+
+    return 0;
+}
